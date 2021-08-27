e973941d15d3ae60935dff774ff97efc3e69b30b
Emma Chory <emmachory@gmail.com>
2021-08-26 20:25:25 -0600

    Create functions to pass tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,41 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+}
 
+
+function createMenuItem(name, price, type){
+  return {name, price, type}
+}
+
+
+function addIngredients(ingredient, arr){
+  if(!arr.includes(ingredient)){
+    arr.push(ingredient);
+  }
+
+}
+
+
+function formatPrice(price) {
+return `$${price}`;
+}
+
+
+function decreasePrice(price) {
+var tenPercent = 10 / 100 * price
+return price - tenPercent
+}
+
+function createRecipe(title, ingredients, type) {
+return {title, ingredients, type}
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
