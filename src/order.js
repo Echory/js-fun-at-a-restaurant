5f3dfb500d9e8e994af50e3c1ae7764ede20422a
Emma Chory <emmachory@gmail.com>
2021-08-27 17:11:36 -0600

    Create function to pass tests order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,26 @@
 
+function takeOrder(order, arr) {
+  if(arr.length < 3){
+  arr.push(order);}
+}
+
+
+function refundOrder(orderToRefund, arr) {
+  var index = arr.findIndex(i => i.orderNumber === orderToRefund)
+ arr.splice(index, 1);
+}
+
+
+function listItems() {
+
+}
+
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
