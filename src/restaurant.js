939e1b66a46f184a82b0c311525b5b66528d4cc0
Emma Chory <emmachory@gmail.com>
2021-08-29 07:52:03 -0600

    Create functions to pass tests restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,40 @@
 
+function createRestaurant(restaurantName) {
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast:[],
+      lunch:[],
+      dinner:[]
+    }
+  }
+  return restaurant
+}
+
+
+
+function addMenuItem(restaurant, item) {
+  if(!restaurant.menus[item.type].includes(item)){
+    restaurant.menus[item.type].push(item);
+  }
+
+}
+
+
+function removeMenuItem(restaurant, itemToRemove, menu) {
+  var indexToRemove = restaurant.menus[menu].findIndex(i => i.name === itemToRemove)
+  if(indexToRemove === -1){
+    return `Sorry, we don't sell ${itemToRemove}, try adding a new recipe!`
+  } else {
+  restaurant.menus[menu].splice(indexToRemove, 1);
+  return `No one is eating our ${itemToRemove} - it has been removed from the ${menu} menu!`
+}
+}
+
+
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
